702eede3c58da748b2e896dc32e00c65c176acf5
Vincent Chow <chow_vincent@utexas.edu>
2017-01-24 00:16:59 -0600

    added non-repeating operators and operator highlighting

--- calculator.js
+++ calculator.js
@@ -1,64 +1,91 @@
-$(function(){
+$(function () {
 	var $allButtons = $("span");
 	var $result = $("#result p");
 	var $clearButton = $("#clear");
 	var equalTapped = false;
-	
+
 	//Clear button
-	$clearButton.on("click", function(){
+	$clearButton.on("click", function () {
 		$result.text('');
 	});
 	//All buttons
-	$allButtons.each(function(){
+	$allButtons.each(function () {
 		var $this = $(this);
-		if($this.text() === "="){
-			$this.on("click", function(){
+		if ($this.text() === "=") {
+			$this.on("click", function () {
 				calculateResult($this);
 			});
-		}else{
-			$this.on("click", function(){
+		} else {
+			$this.on("click", function () {
+				var oldText = $result.text();
+//				console.log(oldText.charAt(oldText.length - 1));				
 				addValue($this);
-			
-//				if($result.text() === ""){
-//					addValue($this);	
-//				}else{
-//					$result.text("");
-//					addValue($this);
-//				}
 			});
 		}
 	});
-	
-	
-	
-	function calculateResult(value){
+	//Compute the result of the input
+	function calculateResult(value) {
 		var oldText = $result.text();
 		$result.text(eval(oldText));
 		equalTapped = true;
 	}
-
-	function addValue(value){
+	//Adds value to the result text
+	function addValue(value) {
 		var oldText = $result.text();
-		if(value.text() === "+"){
-			$result.text(oldText + "+");
+		var lastChar = oldText.charAt(oldText.length - 1);
+		if (value.text() === "+") {
+			removeOperatorHighlight();
+			operatorHighlight(value);
+			printOperator("+", lastChar, oldText)
 			equalTapped = false;
-		}else if(value.text() === "-"){
-			$result.text(oldText + "-");
-			equalTapped = false;			
-		}else if(value.text() === "/"){
-			$result.text(oldText + "/");
-			equalTapped = false;			
-		}else if(value.text() === "x"){
-			$result.text(oldText + "*");
-			equalTapped = false;			
-		}else{
-			if(equalTapped){
+		} else if (value.text() === "-") {
+			removeOperatorHighlight();			
+			operatorHighlight(value);
+			printOperator("-", lastChar, oldText)
+			equalTapped = false;
+		} else if (value.text() === "/") {
+			removeOperatorHighlight();			
+			operatorHighlight(value);
+			printOperator("/", lastChar, oldText)
+			equalTapped = false;
+		} else if (value.text() === "*") {
+			removeOperatorHighlight();			
+			operatorHighlight(value);
+			printOperator("*", lastChar, oldText)
+			equalTapped = false;
+		} else {
+			removeOperatorHighlight();			
+			if (equalTapped) {
 				$result.text(value.text());
 				equalTapped = false;
-			}else{
+			} else {
 				$result.text(oldText + value.text());
 			}
 		}
 	}
+	function printOperator(operator, lastChar, oldText){
+		if(checkOperator(lastChar)){
+			var withoutPrevOperator = oldText.substring(0, oldText.length - 1);
+			$result.text(withoutPrevOperator + operator);						
+		}else{
+			$result.text(oldText + operator);
+		}		
+	}
+	//Returns true if the char is an operator
+	function checkOperator(lastChar){
+		if(lastChar === "+" || lastChar === "-" || lastChar === "/" || lastChar === "*")
+			return true;
+		return false;
+	}
+	//Hightlights the clicked operator
+	function operatorHighlight(value){
+		value.css({"border": "1.4px solid black"});
+	}
+	//Removes the highlighting of the chosen operator
+	function removeOperatorHighlight(){
+		var $operators = $(".operator");
+		$operators.each(function(){
+			$(this).css({"border": "1px solid grey"});
+		});
+	}
 });
-
